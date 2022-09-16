7f3bd18e19ecfe77f7a696ffe8e4855c78f713fb
Agustin <agsp3rs3o@gmail.com>
2022-09-16 02:39:57 -0300

    Finalizando Landing Page

--- Ejercicios Bootstrap/Ejercicios Extras/Landing Page/js/index.js
+++ Ejercicios Bootstrap/Ejercicios Extras/Landing Page/js/index.js
@@ -2,13 +2,22 @@ function show() {
   swal({
     text: "Usted adquirió el curso con exito",
     icon: "success",
+    confirmButtonColor: '#3085d6'
+  });
+}
+
+function registradoExito() {
+  swal({
+    text: "Registrado con exito",
+    icon: "success",
+    confirmButtonColor: '#3085d6'
   });
 }
 
 function showBad() {
   swal({
-    text: "Error. No quedan cursos suficientes",
     icon: "error",
+    title: "Oops...",
+    text: "No poseemos este curso en este momento",
   });
 }
-
